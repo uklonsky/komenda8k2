64b6647166bb05844bf98f87250d0cb80c0f2b53
uklonsky <148268884+uklonsky@users.noreply.github.com>
2023-10-23 03:16:40 +0300

    uploud

---
+++ data.js
@@ -0,0 +1,657 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Двор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1494512699442163,
+        "pitch": -0.19505600993882588,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9791479418639675,
+          "pitch": -0.11029308188625464,
+          "rotation": 0,
+          "target": "1--2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1720984664412635,
+          "pitch": -0.30701834149379614,
+          "title": "Комендантсикй, д.8, к.2",
+          "text": "кирпичный дом"
+        }
+      ]
+    },
+    {
+      "id": "1--2",
+      "name": "Двор 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.3907541704281536,
+        "pitch": 0.11952817153199291,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09742206200303372,
+          "pitch": 0.19925399989991632,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.7030978039248472,
+          "pitch": 0.20121571425659823,
+          "rotation": 0,
+          "target": "2--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6721498506641614,
+          "pitch": -0.27337567080213176,
+          "title": "Вход в парадную",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2--1",
+      "name": "Парадная 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.7448847966273746,
+        "pitch": 0.2347703783211923,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9768385638957344,
+          "pitch": 0.06025726525001929,
+          "rotation": 0,
+          "target": "1--2"
+        },
+        {
+          "yaw": -0.5321915408101177,
+          "pitch": 0.2365415586185513,
+          "rotation": 0,
+          "target": "3-4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5363894481504072,
+          "pitch": -0.39382812452531546,
+          "title": "1 этаж",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-4-",
+      "name": "4 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.9636837490584274,
+        "pitch": 0.12176753568451204,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.138373354134851,
+          "pitch": 0.09382790328784729,
+          "rotation": 0,
+          "target": "4--1"
+        },
+        {
+          "yaw": 1.4020312871264604,
+          "pitch": 0.5706690026182386,
+          "rotation": 0,
+          "target": "2--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--1",
+      "name": "Коридор №1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.41392787013800714,
+        "pitch": 0.35756524684108904,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05561109374905726,
+          "pitch": 0.23382018561651208,
+          "rotation": 0,
+          "target": "3-4-"
+        },
+        {
+          "yaw": -1.0168733796059186,
+          "pitch": 0.32090428708690055,
+          "rotation": 0,
+          "target": "5--1"
+        },
+        {
+          "yaw": 3.1135371021983627,
+          "pitch": 0.12562935892143656,
+          "rotation": 0,
+          "target": "6--2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.05432183988776984,
+          "pitch": -0.01842785236459754,
+          "title": "Выход в парадную",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "5--1",
+      "name": "Санузел №1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8770707234986901,
+        "pitch": 0.9277876817712656,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9089046792336326,
+          "pitch": 0.4627372362797413,
+          "rotation": 0,
+          "target": "4--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--2",
+      "name": "Коридор №2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.6265942783474756,
+        "pitch": 0.38076605483337644,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07906412976686461,
+          "pitch": 0.3994854285266598,
+          "rotation": 0,
+          "target": "8-1"
+        },
+        {
+          "yaw": -1.488607998613194,
+          "pitch": 0.20561107849583848,
+          "rotation": 0,
+          "target": "4--1"
+        },
+        {
+          "yaw": 3.027890463137129,
+          "pitch": 0.46480043138189053,
+          "rotation": 0,
+          "target": "11--3"
+        },
+        {
+          "yaw": 1.5026559937891744,
+          "pitch": 0.1593322134055768,
+          "rotation": 0,
+          "target": "4--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--3",
+      "name": "Комната №3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.1340982784918676,
+        "pitch": 0.1972569936313917,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4947572610031319,
+          "pitch": 0.20319867289869364,
+          "rotation": 0,
+          "target": "11--3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-1",
+      "name": "Зал1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.5070131548765602,
+        "pitch": 0.29892672312306345,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8117994805455488,
+          "pitch": 0.15557165883534552,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": -0.010197515646384048,
+          "pitch": 0.17371598920009212,
+          "rotation": 0,
+          "target": "6--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "Кухня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.5512242322954108,
+        "pitch": 0.406772688933259,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1192142087206971,
+          "pitch": 0.17687593592495787,
+          "rotation": 0,
+          "target": "8-1"
+        },
+        {
+          "yaw": 1.1857473150797588,
+          "pitch": 0.30081805828020336,
+          "rotation": 0,
+          "target": "13---"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9401110778774875,
+          "pitch": -0.25974462188963443,
+          "title": "Кухонный гарнитур",
+          "text": "Дерево - массив дуба"
+        }
+      ]
+    },
+    {
+      "id": "10--2",
+      "name": "Комната №2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.3939994462200502,
+        "pitch": 0.24418277000293287,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10876466923798134,
+          "pitch": 0.20431719829051787,
+          "rotation": 0,
+          "target": "11--3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11--3",
+      "name": "Коридор №3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.6453465792377493,
+        "pitch": 0.2226767410344319,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1254753299193485,
+          "pitch": 0.35979203329513076,
+          "rotation": 0,
+          "target": "10--2"
+        },
+        {
+          "yaw": 1.0884149625430215,
+          "pitch": 0.26723050967283335,
+          "rotation": 0,
+          "target": "7--3"
+        },
+        {
+          "yaw": -3.104159460576593,
+          "pitch": 0.1748343550359408,
+          "rotation": 0,
+          "target": "6--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12--1",
+      "name": "Комната №1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4659983176160356,
+        "pitch": 0.37714204629570247,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3501245829103024,
+          "pitch": 0.21257414629987537,
+          "rotation": 0,
+          "target": "6--2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.09870722435058177,
+          "pitch": -0.014317233864865386,
+          "title": "Балкон №2&nbsp;",
+          "text": "Фото нет. Заставлен вещами"
+        }
+      ]
+    },
+    {
+      "id": "13---",
+      "name": "Балкон с кухни",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.6041703765354978,
+        "pitch": 0.5460905336663977,
+        "fov": 1.5553816896420836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18185861328433006,
+          "pitch": 0.529851290851088,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
